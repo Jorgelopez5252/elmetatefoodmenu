0a1317835d628e50c6cab21e2776f69d262bfcdd
Jorge Lopez <JorgeLopez5252@gmail.com>
2021-05-17 01:45:22 -0700

    updated folder structre and created public folder

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
+
 const PORT = process.env.PORT || 1738;
 
-app.use(express.static(__dirname + '/pages'));
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
